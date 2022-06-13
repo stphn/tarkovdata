358db830121127e6455eff5fa4372c254febacea
stphn <sgoeuriot@gmail.com>
2022-06-13 21:29:50 +0200

    remode axios

--- context/questContext.tsx
+++ context/questContext.tsx
@@ -1,41 +1,10 @@
 import React from 'react'
-import axios from 'axios'
+import allQuests from '../tarkovdata/quests.json'
+const Context = React.createContext<any>(null)
 
-const baseURL = 'https://tarkovtracker.github.io/tarkovdata/quests.json'
-
-const Context = React.createContext<any>([])
-
-type quest = {
-    id: number
-    objectives: [
-        {
-            type: string
-            target: string
-            number: number
-            location: number
-            id: number
-        }
-    ]
-    title: string
-    wiki: string
-    unlocks: string
-    exp: number
-    giver: number
-    require: { level: number; quests: number }
-}
+//console.log(questsData)
 
 function QuestContext({ children }: any) {
-    const [allQuests, setAllQuests] = React.useState<any>([])
-
-    React.useEffect(() => {
-        axios.get(baseURL).then((response) => {
-            setAllQuests(response.data)
-            console.log(response.data)
-        })
-    }, [])
-
-    //if (!setAllQuests) return 'error'
-
     // function toggleFavorite(id: number) {
     //     const updatedArr = allQuests?.map((quest) => {
     //         if (allQuests.id === id) {
