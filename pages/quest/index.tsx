889daa5b46dc6a174ce22067645d640508186d08
stphn <sgoeuriot@gmail.com>
2022-06-13 21:31:35 +0200

    new page

---
+++ pages/quest/index.tsx
@@ -0,0 +1,14 @@
+import styles from '../../styles/Home.module.css'
+
+const Quest = () => {
+    return (
+        <div className={styles.container}>
+            <main className={styles.main}>
+                <h1>Quest:</h1>
+                <div className={styles.grid}>Hello world</div>
+            </main>
+        </div>
+    )
+}
+
+export default Quest
