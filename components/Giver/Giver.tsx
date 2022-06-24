a02f138c683227748dcb5d7fef044962f8d960f4
stphn <sgoeuriot@gmail.com>
2022-06-24 11:07:31 +0200

    cleaned up

--- components/Giver/Giver.tsx
+++ components/Giver/Giver.tsx
@@ -2,12 +2,11 @@ import Image from 'next/image'
 import React from 'react'
 import styles from './Giver.module.scss'
 import traders from '../../tarkovdata/traders.json'
-
 interface GiverProps {
     giver: number
     className?: string
 }
-export const Giver: React.FC<GiverProps> = ({ giver, className }) => {
+export const Giver = ({ giver, className }: GiverProps) => {
     const Givers = (Object.keys(traders) as (keyof typeof traders)[]).map(
         (trader) => {
             const g = traders[trader]
