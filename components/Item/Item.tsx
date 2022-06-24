b44d68eb2fd0baa1596735f0c16876a301da79fc
stphn <sgoeuriot@gmail.com>
2022-06-24 11:08:19 +0200

    cleaned up

--- components/Item/Item.tsx
+++ components/Item/Item.tsx
@@ -1,12 +1,12 @@
-import React, { useContext } from 'react'
-
+import React from 'react'
 import items from '../../tarkovdata/items.en.json'
 
-interface Props {
+// create an interface for the props
+interface ItemProps {
     itemName?: string
     className?: string
 }
-export const Item = ({ itemName, className }: Props) => {
+export const Item = ({ itemName, className }: ItemProps) => {
     const Items = (Object.keys(items) as (keyof typeof items)[]).map((item) => {
         const i = items[item]
         if (itemName === i.id) {
--- components/MenuItem/MenuItem.tsx
+++ components/MenuItem/MenuItem.tsx
@@ -1,7 +1,5 @@
 import Link from 'next/link'
 import React from 'react'
-import { URL } from 'url'
-
 interface Props {
     /**
      * The url
