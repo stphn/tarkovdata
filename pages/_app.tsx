bbc3145f222df37a890b1948d60a0bb7ff4007e6
stphn <sgoeuriot@gmail.com>
2022-06-24 11:11:45 +0200

    imported as default

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.scss'
 
 import type { AppProps } from 'next/app'
-import Sidebar from '../components/Sidebar/Sidebar'
+import { Sidebar } from '../components/Sidebar/Sidebar'
 import { ThemeProvider } from '../context/themeContext'
 import { UserProvider } from '../context/userContext'
 
