9690510f03469a8fddadb91aebc165640c00769d
stphn <sgoeuriot@gmail.com>
2022-06-26 07:16:16 +0200

    moved as a separate global file

--- components/Quest/QuestProps.tsx
+++ components/Quest/QuestProps.tsx
@@ -1,21 +1,58 @@
 export interface QuestProps {
-    id?: number
-    objectives?: [
-        {
-            type: string
-            target: string
-            tool: string
-            hint: string
-            number: number
-            location: number
-            id: number
-        }
-    ]
-    locales?: { en: string; ru: string; cs: string }
-    title?: string
-    wiki?: string
-    unlocks?: string
-    exp?: number
+    /**The id of the quest */
+    id: number
+    /**The optional requirements before completing the quest */
+    require: QuestRequire
+    /**The trader id who gave that quest */
     giver: number
-    require: { level: number; quests: [number] }
+    turnin: number
+    title: string
+    locales: {
+        en: string
+        ru: string
+        cs: string
+    }
+    wiki: string
+    exp: number
+    nokappa?: boolean
+    unlocks: string[]
+    reputation: QuestReputation[]
+    reputationFailure: QuestReputation[]
+    objectives: QuestObjective[]
+    alternatives: number[]
+    gameId: string
+}
+export interface QuestObjective {
+    /**The kind of quest */
+    type?: string
+    /**The Target of the Quest */
+    target?: string
+    tool?: string
+    hint?: string
+    have?: number
+    with?: string[]
+    /**The amount of target*/
+    number?: number
+    location?: number
+    id?: number
+    gps?: {
+        leftPercent: number
+        topPercent: number
+        floor: string
+    }
+    map?: number
+}
+
+interface QuestReputation {
+    trader: number
+    rep: number
+}
+
+interface QuestRequire {
+    level: number
+    quests: number[]
+}
+interface QuestReputationFailure {
+    trader: number
+    rep: number
 }
--- components/Quest/index.ts
+++ components/Quest/index.ts
@@ -1,1 +1,2 @@
 export * from './Quest'
+export * from './QuestProps'
