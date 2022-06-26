91c5b660fc2f61062fcc13b18d568b7feba1e8ce
stphn <sgoeuriot@gmail.com>
2022-06-26 07:12:20 +0200

    changed props location and names

--- components/Location/Location.tsx
+++ components/Location/Location.tsx
@@ -1,15 +1,13 @@
+import { QuestObjective } from '../Quest'
 import React from 'react'
 import maps from '../../tarkovdata/maps.json'
 
-type Props = {
-    mapName?: number
-}
-export const Location: React.FC<Props> = ({ mapName }) => {
+export const Location = ({ map }: QuestObjective) => {
     const Locations = (Object.keys(maps) as (keyof typeof maps)[]).map(
         (location) => {
             const l = maps[location]
-            if (mapName === l.id) {
-                return <span key={l.id}> on {l.locale.en}</span>
+            if (map === l.id) {
+                return <span key={l.id}>&nbsp;on {l.locale.en}</span>
             }
         }
     )
