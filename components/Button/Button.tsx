0dc49e4f9a802a4d00081d5ffb081418d1f73825
stphn <sgoeuriot@gmail.com>
2022-06-26 07:43:58 +0200

    better indentation of className

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -10,18 +10,7 @@ export const Button = ({ onClick, children, className }: ButtonProps) => {
     return (
         <button
             type='button'
-            className={`py-4
-   px-10
-   lg:px-8
-   xl:px-10
-   inline-flex
-   items-center
-   justify-center
-   text-center text-primary text-base
-   border border-primary
-   rounded-md
-   hover:bg-primary hover:border-primary hover:text-white
-   transition ${className}`}
+            className={`py-4 px-10 lg:px-8 xl:px-10 inline-flex items-center justify-center text-center text-primary text-base border border-primary rounded-md hover:bg-primary hover:border-primary hover:text-white transition ${className}`}
             onClick={onClick}
         >
             {children}
