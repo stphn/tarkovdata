828cf64f502aee2853836bddd708b7dc3b9e0ff4
stphn <sgoeuriot@gmail.com>
2022-06-26 07:14:46 +0200

    calling the localhost moved to the quest component

--- components/Quests/Quests.tsx
+++ components/Quests/Quests.tsx
@@ -1,30 +1,13 @@
-import { Quest, QuestProps } from '../Quest/Quest'
+import { Quest, QuestProps } from '../Quest'
 import React, { useContext } from 'react'
 
 import { Context } from '../../context/questContext'
-import useLocalStorage from '@rehooks/local-storage'
 
 export const Quests = () => {
     const { allQuests } = useContext(Context)
-    const [level] = useLocalStorage<number>('level' || 1)
 
-    const questElements = allQuests.map((quest: QuestProps) => {
-        if (level && level >= quest.require.level) {
-            return (
-                <Quest
-                    key={quest.id}
-                    title={quest.title}
-                    objectives={quest.objectives}
-                    giver={quest.giver}
-                    exp={quest.exp}
-                    wiki={quest.wiki}
-                    locales={quest.locales}
-                    require={quest.require}
-                    id={quest.id}
-                    nokappa={quest.nokappa}
-                />
-            )
-        }
+    const questElements = allQuests.map((quest: QuestProps, id: number) => {
+        return <Quest key={id} {...quest} />
     })
     return <div>{questElements}</div>
 }
