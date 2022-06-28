3210a3b7a6567b36f595e264db2566ad960dfff4
stphn <sgoeuriot@gmail.com>
2022-06-28 08:30:14 +0200

    added more options

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
 
 interface ButtonProps {
-    children: string | React.ReactNode
+    children?: string | React.ReactNode
+    icon?: React.ReactNode
     onClick: () => void
     className?: string
 }
 
-export const Button = ({ onClick, children, className }: ButtonProps) => {
+const buttonClasses =
+    'inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'
+
+export const Button = ({ onClick, children, className, icon }: ButtonProps) => {
     return (
         <button
             type='button'
-            className={`py-4 px-10 lg:px-8 xl:px-10 inline-flex items-center justify-center text-center text-primary text-base border border-primary rounded-md hover:bg-primary hover:border-primary hover:text-white transition ${className}`}
             onClick={onClick}
+            className={buttonClasses + className}
         >
-            {children}
+            <div className='flex flex-between'>
+                <span className='mr-1'>{icon}</span>
+                <span>{children}</span>
+            </div>
         </button>
     )
 }
